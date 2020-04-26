93f5baba3bc2c1c2faab4d4a109b50dc18ce800d
Alex <slicer256@gmail.com>
2020-04-26 20:28:40 +0300

    use separate component for cars list

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,26 +1,19 @@
 <template>
     <Page>
         <ActionBar title="Dillish Cars"/>
-        <ScrollView>
-            <StackLayout class="cars-list">
-                <GridLayout v-for="car in cars" columns="100, *" rows="auto, auto" class="cars-list-item">
-                    <Image :src="car.image" stretch="aspectFill" row="0" rowSpan="2" col="0" />
-                    <Label :text="car.manufacturer + ' ' + car.model" row="0" col="1" />
-                    <Label :text="car.price" row="1" col="1" />
-                </GridLayout>
-            </StackLayout>
-        </ScrollView>
+        <CarsList />
     </Page>
+
 </template>
 
 <script >
+import CarsList from './CarsList';
+
 export default {
+    components: { CarsList },
     data() {
         return {
-            cars: [
-                {manufacturer: 'BMW', model: 'Model 1', image: 'https://dillishcars.com/upload/cars_na/20268/20268_300_1.jpg', price: 100000},
-                {manufacturer: 'Volkswagen', model: 'Model 2', image: 'https://dillishcars.com/upload/cars_na/20269/20269_300_1.jpg', price: 100000}
-            ],
+
         }
     },
     created() {
@@ -34,21 +27,4 @@ export default {
         background-color: #53ba82;
         color: #ffffff;
     }
-
-    .cars-list {
-        vertical-align: top;
-        font-size: 20;
-        margin: 10;
-    }
-
-    .cars-list-item {
-        margin-bottom: 10;
-    }
-
-    .cars-list Image {
-        border-radius: 5;
-        margin-right: 7;
-    }
-
-
 </style>
